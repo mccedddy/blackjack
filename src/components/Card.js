aac9c2cd0d855927eb68ba04120c5892ef82269c
mccedddy <cedricmacallan@gmail.com>
2024-07-03 17:44:46 +0800

    Added player hands, hit, unflip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Card({ value }) {
-  const cardImage = value
-    ? `${process.env.PUBLIC_URL}/assets/cards/${value}.png`
-    : `${process.env.PUBLIC_URL}/assets/cards/cardBack.png`;
+  // const cardImage = value
+  //   ? `${process.env.PUBLIC_URL}/assets/cards/${value}.png`
+  //   : `${process.env.PUBLIC_URL}/assets/cards/cardBack.png`;
+
+  var cardImage = `${process.env.PUBLIC_URL}/assets/cards/cardBack.png`;
+
+  if (value && !value.endsWith("0")) {
+    cardImage = `${process.env.PUBLIC_URL}/assets/cards/${value}.png`;
+  }
 
   return (
     <img
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,8 @@ import Button from "./Button";
 function Game({ handleForfeitClick, bet, money, setMoney }) {
   const [isActionVisible, setActionVisible] = useState(true);
   const [stack, setStack] = useState([]);
-  const [currentCard, setCurrentCard] = useState(null);
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
 
   const deck = [
     "1c",
@@ -62,46 +63,73 @@ function Game({ handleForfeitClick, bet, money, setMoney }) {
     "13d",
     "13h",
   ];
+
   function shuffle(array) {
     let currentIndex = array.length,
       randomIndex;
-
-    // While there remain elements to shuffle...
     while (currentIndex !== 0) {
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex],
       ];
     }
-
     return array;
   }
-  function drawCard() {
+
+  async function drawCard(handSetter, flipped = false) {
     setStack((prevStack) => {
       const newStack = [...prevStack];
-      const card = newStack.shift();
-      setCurrentCard(card);
+      var card = newStack.shift();
+      if (flipped) {
+        card += "0";
+      }
+      console.log(card);
+      handSetter((prevHand) => [...prevHand, card]);
       return newStack;
     });
   }
 
+  function unflipCard(card) {
+    if (card.endsWith("0")) {
+      return card.slice(0, -1);
+    }
+    return card;
+  }
+
+  function revealCards() {
+    setDealerHand((prevHand) => {
+      const newHand = [...prevHand];
+      newHand[newHand.length - 1] = unflipCard(newHand[newHand.length - 1]);
+      return newHand;
+    });
+  }
+
+  // Initialize
   useEffect(() => {
-    // This effect runs once on mount to shuffle deck and set stack
-    const shuffledDeck = shuffle([...deck]);
-    setStack(shuffledDeck);
+    const initializeGame = async () => {
+      const shuffledDeck = shuffle([...deck]);
+      setStack(shuffledDeck);
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      await drawCard(setPlayerHand);
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      await drawCard(setDealerHand);
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      await drawCard(setPlayerHand);
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      await drawCard(setDealerHand, true);
+    };
+
+    initializeGame();
   }, []);
 
   useEffect(() => {
-    if (currentCard) {
-      console.log("Card:", currentCard);
-    }
-  }, [currentCard]);
+    console.log("Player Hand:", playerHand);
+    console.log("Dealer Hand:", dealerHand);
+  }, [playerHand, dealerHand]);
 
+  // Log stack
   useEffect(() => {
     if (stack.length !== 0) {
       console.log("Stack:", stack);
@@ -115,8 +143,10 @@ function Game({ handleForfeitClick, bet, money, setMoney }) {
       {/* Dealer section */}
       <div className="flex flex-col gap-2 items-center w-full">
         <Container text="Dealer" />
-        <div className="flex space-x-1">
-          <Card />
+        <div id="dealerHand" className="flex space-x-1">
+          {dealerHand.map((card, index) => (
+            <Card key={index} value={card} />
+          ))}
         </div>
         <Container text="0" />
       </div>
@@ -125,12 +155,23 @@ function Game({ handleForfeitClick, bet, money, setMoney }) {
       {isActionVisible && (
         <div className="w-auto h-auto flex gap-2 items-center justify-center bg-white p-4 rounded-lg shadow-modalShadow">
           <Button text="2" color="bg-orangeButton" />
-          <Button text="+" color="bg-greenButton" onClick={drawCard} />
+          <Button
+            text="+"
+            color="bg-greenButton"
+            onClick={() => drawCard(setPlayerHand)}
+          />
           <Button text="-" color="bg-redButton" />
           <Button
             text="F"
             color="bg-blackButton"
             onClick={handleForfeitClick}
+          />
+          <Button
+            text="Unflip"
+            color="bg-blackButton"
+            onClick={() => {
+              revealCards();
+            }}
           />
         </div>
       )}
@@ -138,8 +179,10 @@ function Game({ handleForfeitClick, bet, money, setMoney }) {
       {/* Player section */}
       <div className="flex flex-col gap-2 items-center mb-4 w-full">
         <Container text="0" />
-        <div className="flex space-x-1">
-          <Card />
+        <div id="playerHand" className="flex space-x-1">
+          {playerHand.map((card, index) => (
+            <Card key={index} value={card} />
+          ))}
         </div>
         <Container text="Player" />
       </div>
